70627b191ed695f7b5db87d96e1f39e9bc76d0d7
LaPluma37 <helinyi37@gmail.com>
2023-09-26 20:31:05 +0800

    add backend

--- picture-classify/server.py
+++ picture-classify/server.py
@@ -1,5 +1,22 @@
-import sanic
+from sanic import Sanic
 from sanic.response import json
+from sanic.response import text
+
+app = Sanic(__name__)
 
 
-app = sanic.Sanic(__name__)
+@app.route("/data")
+async def get_data(request):
+    with open("data.json", "r") as f:
+        data = f.read()
+        print(data)
+    return json(data)
+
+
+@app.route("/")
+async def hello_world(request):
+    return text("Hello, world.")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5391, debug=False)
